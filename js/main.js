fd1d5358f2eb36f7049d8d97efe9b1a3effb950f
Dmitry Bogutskii <bogutskii.d@gmail.com>
2019-05-19 17:08:17 +0300

    fixed js7

--- js/main.js
+++ js/main.js
@@ -1,25 +1,39 @@
 'use strict';
 
 function showList(list) {
+    let array = [];
+    let newArray = [];
+    let finalArray = [];
+    function traverse(theObject) {
 
-    let newArray = list.map(function(element) {
+        for (let key in theObject) {
+            if (theObject[key] != null && typeof (theObject[key]) == 'object') {
+                traverse(theObject[key]);
 
-        let listNode = document.getElementById('list');
-        let liNode = document.createElement('LI');
-        let textNode = document.createTextNode(element);
-        liNode.appendChild(textNode);
-        listNode.appendChild(liNode);
-        return element;
-    });
-    console.log(newArray);
+            } else {
+
+                array.push(theObject[key]);
+                newArray = array.map(word => `<li>${word}</li>`);
+                finalArray = newArray.join('');
+                document.body.children[2].innerHTML = finalArray;
+            }
+        }
+    }
+    traverse(list);
+
     let i = 10;
     let timerId = setInterval(function() {
         document.getElementById("timer").innerText = i;
-        if (i == 0) clearInterval(timerId);
+        if (i === 0) {
+        document.getElementById('list').classList.add("none");
+        document.getElementById('timer').classList.add("none");
+        clearInterval(timerId);
+        newArray = [];
+        }
 
         i--;
     }, 1000);
-    newArray = [];
+
 }
 
 showList(['hello', 'world', {name: "Dima", sName: "Bog"}, 'Kiev', 'Kharkiv', 'Odessa', 'Lviv']);
